5ed1be145e8cb59cf231f9b7153e6988dfe227fb
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-04-28 08:14:32 -0500

    initial commit

---
+++ api.js
@@ -0,0 +1,18 @@
+module.exports = {
+  getUsers: function () {
+    return [
+      {
+        name: 'janai',
+        age:'08'
+      },
+      {
+        name: 'logan',
+        age:'02'
+      },
+      {
+        name: 'adrian',
+        age:'35'
+      }
+    ]
+  }
+}

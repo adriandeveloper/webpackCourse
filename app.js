dfd4766d5529531d2f0850601308d5fa53b494f8
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-04-28 08:19:40 -0500

    adds es5 syntax to double check if error persists

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var $ = require('jquery');
 
 var users = api.getUsers();
 
- console.log($);
-// $.each(users, function(index, user) {
-//   $.(document.body).append(index + "<p> name " + user.name + ' age ' + user.age + "</p>");
-// })
+ // console.log($);
+$.each(users, function(index, user) {
+  $.(document.body).append(index + "<p> name " + user.name + ' age ' + user.age + "</p>");
+})
